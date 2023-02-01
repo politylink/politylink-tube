5d8097d170f33f08a85dce97f615e931b0dda860
MitsukiUsui <mitsuo.com.mu@gmail.com>
2023-02-01 13:36:42 +0900

    update whisper path

---
+++ README.md
@@ -0,0 +1,21 @@
+## setup
+
+### FFmpeg
+```
+brew install ffmpeg
+```
+
+### Whisper.cpp
+https://github.com/ggerganov/whisper.cpp
+
+```
+git clone https://github.com/ggerganov/whisper.cpp.git
+cd whisper.cpp
+make
+bash ./models/download-ggml-model.sh large
+```
+
+Add 'WHISPER_ROOT' environment variable.
+```
+export WHISPER_ROOT=${DIR}
+```
--- do_transcribe.py
+++ do_transcribe.py
@@ -1,12 +1,13 @@
 import argparse
 import logging
+import os
 import re
 from pathlib import Path
 
-import pandas as pd
 import requests
 
 from mylib.command import CommandTask
+from mylib.ffmpeg import read_silence_df, to_segment_df
 
 LOGGER = logging.getLogger(__name__)
 
@@ -21,102 +22,65 @@ def get_m3u8_url(video_url):
         raise ValueError(f'failed to extract m3u8 url from {video_url}')
 
 
-def read_silence_df(silence_fp):
-    """
-    parse silence detection result by FFmpeg
-    return (start, end) of detected silence
-    """
-
-    with open(silence_fp, 'r') as f:
-        lines = f.readlines()
-
-    records = []
-    start = None
-    pattern = r'silence_(start|end): ([0-9.]+)'
-    for line in lines:
-        match = re.search(pattern, line)
-        if match:
-            is_start = match.group(1) == 'start'
-            time = float(match.group(2))
-            if is_start:
-                start = time
-            else:
-                records.append({'start': start, 'end': time})
-                start = None
-    silence_df = pd.DataFrame(records)
-    return silence_df
+def build_download_task(m3u8_url, video_fp, log_fp):
+    cmd = f'ffmpeg -i {m3u8_url} {video_fp}'
+    return CommandTask(cmd, log_fp)
 
 
-def to_segment_df(silence_df):
-    """
-    inverse silence_df to
-    """
-
-    start = 0
-    records = []
-    for _, row in silence_df.iterrows():
-        if row['start'] > 0:
-            records.append({
-                'segment_id': len(records),
-                'start': int(start),
-                'end': int(row['start']) + 1
-            })
-        start = row['end']
-    segment_df = pd.DataFrame(records)
-    return segment_df
-
-
-def build_download_task(m3u8_url, mp3_fp, log_fp):
-    cmd = f'ffmpeg -i {m3u8_url} {mp3_fp}'
-    task = CommandTask(cmd, log_fp)
-    return task
+def build_audio_task(video_fp, audio_fp):
+    cmd = f'ffmpeg -i {video_fp} {audio_fp}'
+    return CommandTask(cmd)
 
 
 def build_silence_task(audio_fp, out_fp):
-    cmd = f'ffmpeg -i {audio_fp} -af silencedetect=d=10 -f null -'
-    task = CommandTask(cmd, out_fp)
-    return task
+    cmd = f'ffmpeg -i {audio_fp} -af silencedetect=d=10:n=-10dB -f null -'
+    return CommandTask(cmd, out_fp)
 
 
 def build_split_task(audio_fp, start, end, out_fp):
     duration = end - start
     cmd = f'ffmpeg -y -ss {start} -i {audio_fp} -t {duration} -ar 16000 -ac 1 -c:a pcm_s16le {out_fp}'
-    task = CommandTask(cmd)
-    return task
+    return CommandTask(cmd)
 
 
-def build_transcribe_task(wav_fp):
-    whisper_direc = '/Users/mitsuki/sandbox/whisper.cpp'
-    bin_fp = f'{whisper_direc}/main'
-    model_fp = f'{whisper_direc}/models/ggml-small.bin'
+def build_transcribe_task(wav_fp, log_fp):
+    whisper_dir = Path(os.environ['WHISPER_ROOT'])
+    bin_fp = whisper_dir / 'main'
+    model_fp = whisper_dir / 'models/ggml-large.bin'
     cmd = f'{bin_fp} --model {model_fp} --language ja --file {wav_fp} --output-csv'
-    task = CommandTask(cmd)
-    return task
+    return CommandTask(cmd, log_fp)
 
 
 def main():
-    m3u8_url = get_m3u8_url(args.input)
-
     log_dir = Path(args.output) / str(args.job) / 'log'
-    audio_dir = Path(args.output) / str(args.job) / 'audio'
+    data_dir = Path(args.output) / str(args.job) / 'data'
+
     log_dir.mkdir(parents=True, exist_ok=True)
-    audio_dir.mkdir(parents=True, exist_ok=True)
-    full_fp = audio_dir / 'full.mp3'
-    silence_fp = audio_dir / 'silence.csv'
+    data_dir.mkdir(parents=True, exist_ok=True)
 
-    build_download_task(m3u8_url, full_fp, log_dir / 'download.log').run()
-    build_silence_task(full_fp, silence_fp).run()
+    m3u8_url = get_m3u8_url(args.input)
+    mp4_fp = data_dir / 'video.mp4'
+    mp3_fp = data_dir / 'audio.mp3'
+    log_fp = log_dir / 'download.log'
+    silence_fp = data_dir / 'silence.log'
+    segment_fp = data_dir / 'segment.csv'
 
+    if not mp4_fp.exists():
+        build_download_task(m3u8_url, mp4_fp, log_fp).run()
+    if not mp3_fp.exists():
+        build_audio_task(mp4_fp, mp3_fp).run()
+
+    build_silence_task(mp3_fp, silence_fp).run()
     silence_df = read_silence_df(silence_fp)
     segment_df = to_segment_df(silence_df)
-    LOGGER.info(f'found {len(segment_df)} segments')
-    segment_fp = audio_dir / 'segment.csv'
     segment_df.to_csv(segment_fp, index=False)
+    LOGGER.info(f'found {len(segment_df)} segments')
 
     for _, row in segment_df.iterrows():
-        seg_fp = audio_dir / '{}.wav'.format(row['segment_id'])
-        build_split_task(full_fp, row['start'], row['end'] - row['start'], seg_fp).run()
-        build_transcribe_task(seg_fp).run()
+        seg_fp = data_dir / '{}.wav'.format(row['segment_id'])
+        log_fp = str(seg_fp) + '.log'
+        build_split_task(mp3_fp, row['start'], row['end'], seg_fp).run()
+        build_transcribe_task(seg_fp, log_fp).run()
 
 
 if __name__ == '__main__':
