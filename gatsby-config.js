6a9f12e6c31bf38e985de9ccea1e653a2c1a3725
MitsukiUsui <mitsuo.com.mu@gmail.com>
2023-02-01 16:26:58 +0900

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `politylink-video`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
