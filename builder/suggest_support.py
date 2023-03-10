bba0512a7c51f20a27404fbbc133c32bd14ceeca
MitsukiUsui <mitsuo.com.mu@gmail.com>
2023-03-10 12:09:32 +0900

    dev: suggest_support

--- builder/suggest_support.py
+++ builder/suggest_support.py
@@ -1,18 +1,16 @@
-from pathlib import Path
-
 from do_transcribe import build_requests
 from mylib.utils.path import PathHelper
-from mylib.workflow.transcribe import TranscribeJobScheduler, WhisperJob
+from mylib.workflow.jobs import AudioDownloadJob
+from mylib.workflow.transcribe import TranscribeJobScheduler
 
 
 def main():
     scheduler = TranscribeJobScheduler(path_helper=PathHelper())
     requests = build_requests()
     jobs = scheduler.schedule_batch(requests)
-    jobs = list(filter(lambda x: isinstance(x, WhisperJob), jobs))[::-1]
+    jobs = list(filter(lambda x: isinstance(x, AudioDownloadJob), jobs))[::-1]
     for job in jobs:
-        wav_fp = Path(job.context.class_kwargs['wav_fp']).absolute()
-        print(wav_fp)
+        print(job.bash_command)
 
 
 if __name__ == '__main__':
